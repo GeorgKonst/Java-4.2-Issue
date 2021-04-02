6929d0196277752d84061b497ac7ff1d7ac031a7
Georgy Levochkin <aspect71rus@gmail.com>
2021-04-02 11:12:37 +0300

    v2

--- src/main/java/ru/netology/manager/Manager.java
+++ src/main/java/ru/netology/manager/Manager.java
@@ -5,6 +5,7 @@ import ru.netology.repository.Repository;
 
 import java.util.*;
 import java.util.Comparator;
+import java.util.function.Predicate;
 
 public class Manager {
     private Repository repository;
@@ -29,16 +30,55 @@ public class Manager {
         return repository.findClosed();
     }   // Отобразить закрытые задачи
 
-    public void getAll(){
-        Comparator issueComparator = new Comparator();
+    public void closedIssue(int id){
+        repository.closeById(id);
+    }
+    public void openIssue(int id){
+        repository.openById(id);
+    }
+
+    public List<Issue> sortByNew(){
         List<Issue> result = new ArrayList<>();
         result.addAll(repository.getAll());
         Collections.sort(result);
-        for (int i=0; i < result.size() ; i++) {
-            System.out.println(result.get(i));
-        }
-
+        return result;
     }
 
+    public List<Issue> sortByOld(){
+        Comparator ByOld = Comparator.reverseOrder();
+        List<Issue> issues = new ArrayList<>();
+        issues.addAll(repository.getAll());
+        issues.sort(ByOld);
+        return issues;
+    }
 
+    public List<Issue> filterByAuthor(String author){
+        List<Issue> result = new ArrayList<>();
+        Predicate<String> predicate = Predicate.isEqual(author);
+        for (Issue item : repository.getAll())
+            if (predicate.test(item.getAuthor())){
+                result.add(item);
+            };
+        return result;
+    }
+
+    public List<Issue> filterByLabel(String label){
+        List<Issue> result = new ArrayList<>();
+        Predicate<Set<String>> predicate = x -> x.contains(label);
+        for (Issue item : repository.getAll())
+            if (predicate.test(item.getLabel())){
+                result.add(item);
+            }
+        return result;
+    }
+
+    public List<Issue> filterByAssignee(String assignee){
+        List<Issue> result = new ArrayList<>();
+        Predicate<Set<String>> predicate = x -> x.contains(assignee);
+        for (Issue item : repository.getAll())
+            if (predicate.test(item.getAssignee())){
+                result.add(item);
+            }
+        return result;
+    }
 }
